31bf0dfede2dc2967649328db25030147bd7adf9
Gwendal Boedec <boedec.g@gmail.com>
2025-03-18 16:13:22 +0100

    add one gamestyle in game model

--- config/GAMESTYLE_PROPERTY.js
+++ config/GAMESTYLE_PROPERTY.js
@@ -1,6 +1,6 @@
 const GAMESTYLE_PROPERTY = {
     type: [String],
-    enum: ["adventure", "fight", "FPS", "platform", "racing", "RPG", "strategy"],
+    enum: ["adventure", "fight", "FPS", "platform", "racing", "RPG", "sport", "strategy"],
 }
 
 module.exports = GAMESTYLE_PROPERTY
